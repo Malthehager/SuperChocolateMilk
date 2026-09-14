5439b47e9762776eb524a7df129f8a91646b37a5
malthehager <malhag01@easv365.dk>
2026-09-14 12:57:30 +0200

    added milkrecipecalculator xunit tests (closses

--- SuperChocolateMilk.UnitTests/UnitTest1.cs
+++ SuperChocolateMilk.UnitTests/UnitTest1.cs
@@ -1,10 +1,31 @@
 ﻿namespace SuperChocolateMilk.UnitTests;
 
-public class UnitTest1
+using Xunit;
+using SuperChocolateMilk.Core;
+
+public class MilkRecipeCalculatorTests
 {
     [Fact]
-    public void Test1()
+    public void CalculateChocolateSyrup_RegularRichness_ReturnsTenPercentRatio()
     {
+        int milkVolume = 1000;
+        string richness = "REGULAR";
 
+        decimal result = MilkRecipeCalculator.CalculateChocolateSyrupRequired(milkVolume, richness);
+
+        Assert.Equal(100m, result);
+    }
+
+    [Theory]
+    [InlineData(1000, "LIGHT", 75)]
+    [InlineData(1000, "EXTRA", 150)]
+    [InlineData(1000, "ULTRA_CHOCO", 200)]
+    [InlineData(0, "REGULAR", 0)]
+    public void CalculateChocolateSyrup_VariousScenarios_ReturnsExpectedAmount(
+        int milkVolume, string richness, decimal expectedSyrup)
+    {
+        decimal actualResult = MilkRecipeCalculator.CalculateChocolateSyrupRequired(milkVolume, richness);
+
+        Assert.Equal(expectedSyrup, actualResult);
     }
 }
